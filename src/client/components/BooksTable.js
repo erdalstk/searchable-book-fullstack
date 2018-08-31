5e7034cfa09761c79047ed13524e8f9765d403bd
Nguyen, Hao | Hao | ECMD <hao.nguyen@rakuten.com>
2018-08-31 12:18:08 +0900

    apply react-bootstrap-table

--- src/client/components/BooksTable.js
+++ src/client/components/BooksTable.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import './BooksTable.css';
 
-const BookRow = props => {
-  return (
-    <tr>
-      <td>
-        <Link to={'/books/' +  props.p.id}>{props.p.name}</Link>
-        {/* <a href="#">{props.p.name}</a> */}
-      </td>
-      <td>{props.p.author}</td>
-      <td>...</td>
-      <td>
-        <img
-          className="books-table-image"
-          src={props.p.image}
-          alt={props.p.name}
-        />
-      </td>
-    </tr>
-  );
-};
-
 class BooksTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.options = {
+      defaultSortName: 'name', // default sort column name
+      defaultSortOrder: 'asc' // default sort order
+    };
+  }
+
+  imageFormatter(cell, row) {
+    return (
+      "<img class='books-table-image' alt='" +
+      row.name +
+      "' src='" +
+      cell +
+      "'/>"
+    );
+  }
+
+  nameFormatter(cell, row) {
+    return <Link to={'/books/' + row.id}>{row.name}</Link>;
+  }
+
+  categoryFormatter(cell, row) {
+    return 'Văn học';
+  }
+
   render() {
     const rows = [];
     if (!this.props.results.length) {
@@ -34,22 +40,44 @@ class BooksTable extends React.Component {
       );
     }
 
-    this.props.results.forEach(p => {
-      rows.push(<BookRow p={p} key={p.id} />);
-    });
-
     return (
-      <table className="books-table table table-hover" id="dtBasicExample" >
-        <thead className="thead">
-          <tr>
-            <th scope="col">Author</th>
-            <th scope="col">Name</th>
-            <th scope="col">Category</th>
-            <th scope="col">description</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
+      <BootstrapTable
+        data={this.props.results}
+        options={this.options}
+        hover
+        pagination
+        className="books-table"
+      >
+        {/* <TableHeaderColumn isKey dataField='id'>#</TableHeaderColumn> */}
+        <TableHeaderColumn
+          isKey
+          dataField="name"
+          dataFormat={this.nameFormatter}
+          dataSort
+          width='30%'
+        >
+          Name
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="author" width='20%'>Author</TableHeaderColumn>
+        <TableHeaderColumn
+          dataField="description"
+          dataFormat={this.categoryFormatter}
+          width='25%'
+        >
+          Description
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          dataField="category"
+          dataFormat={this.categoryFormatter}
+          dataSort
+          width='15%'
+        >
+          Category
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="image" dataFormat={this.imageFormatter} width='10%'>
+          Cover
+        </TableHeaderColumn>
+      </BootstrapTable>
     );
   }
 }
