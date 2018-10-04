6f8360f1e95ae5475da1a4fa209eaaf411692e52
Nguyen, Hao | Hao | ECMD <hao.nguyen@rakuten.com>
2018-10-04 09:47:19 +0900

    [COMPLETE PHRASE 1] Close development phrase 1
    * PH1 IS ENDED *
    - Completed all essential features (search books, view details, category, upload book, edit book, user auth)
    - Add Admin features (book manage, user manage)
    
    * TODO Ph2 *
    - Migrate to NextJS to support server side rendering
    - Migrate to TypeScript

--- src/client/components/presentational/Footer.js
+++ src/client/components/presentational/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
       <div className="row">
         <div className="col-md-6 mt-md-0 mt-3">
           <h5 className="text-uppercase">About us</h5>
-          <p>MDSTBooks! v0.51b</p>
+          <p>MDSTBooks! v0.53b</p>
           <ul className="list-unstyled">
             <li>
               <Link to="/privacy">Privacy Policy</Link>
